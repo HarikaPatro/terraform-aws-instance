b20b5cdec006f410402942c2c3a217a1dec22a3c
HarikaPatro <patroharika866@gmail.com>
2026-05-28 18:45:30 +0530

    module

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "terraform" {
+resource "aws_instance" "this" {
   ami           = var.ami_id #mandatory
   instance_type = var.instance_type  #mandatory
   vpc_security_group_ids = var.sg_ids  #mandatory
